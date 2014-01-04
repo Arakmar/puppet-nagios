faa6ad9da3f992291f914315512bcd80559c66a6
Yoann Laissus <yoann.laissus@gmail.com>
2014-01-04 13:33:22 +0100

    Convert remaining classes to be server dependant

--- manifests/command/nrpe.pp
+++ manifests/command/nrpe.pp
@@ -1,14 +1,18 @@
-class nagios::command::nrpe {
+class nagios::command::nrpe(
+    $server_name = "default"
+) {
 
   # this command runs a program $ARG1$ with arguments $ARG2$
   nagios::type::command {
     'check_nrpe':
-       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -a $ARG2$'
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -a $ARG2$',
+       server_name => $server_name
   }
 
   # this command runs a program $ARG1$ with no arguments
   nagios::type::command {
     'check_nrpe_1arg':
-       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$'
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$',
+       server_name => $server_name
   }
 }
--- manifests/command/nrpe_timeout.pp
+++ manifests/command/nrpe_timeout.pp
@@ -1,11 +1,15 @@
-class nagios::command::nrpe_timeout {
+class nagios::command::nrpe_timeout(
+    $server_name = "default"
+) {
   nagios::type::command {
     'check_nrpe_timeout':
-       command_line => '/usr/lib/nagios/plugins/check_nrpe -t $ARG1$ -H $HOSTADDRESS$ -c $ARG2$ -a $ARG3$'
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -t $ARG1$ -H $HOSTADDRESS$ -c $ARG2$ -a $ARG3$',
+       server_name => $server_name
   }
 
   nagios::type::command {
     'check_nrpe_1arg_timeout':
-       command_line => '/usr/lib/nagios/plugins/check_nrpe -t $ARG1$ -H $HOSTADDRESS$ -c $ARG2$'
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -t $ARG1$ -H $HOSTADDRESS$ -c $ARG2$',
+       server_name => $server_name
   }
 }
