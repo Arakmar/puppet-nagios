3c05199ebd5a5d62c0f94027f420037cff01b2d7
Yoann Laissus <yoann.laissus@gmail.com>
2015-12-15 23:00:46 +0100

    Revert command/* changes for plugin path. Not ready.

--- manifests/command/nrpe.pp
+++ manifests/command/nrpe.pp
@@ -5,25 +5,25 @@ class nagios::command::nrpe(
   # this command runs a program $ARG1$ with arguments $ARG2$
   nagios::type::command {
     'check_nrpe':
-       command_line => "${nagios::nrpe::plugindir}/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -a $ARG2$",
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -a $ARG2$',
        server_name => $server_name
   }
   
   nagios::type::command {
     'check_nrpe_port':
-       command_line => "${nagios::nrpe::plugindir}/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -p $ARG2$ -a $ARG3$",
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -p $ARG2$ -a $ARG3$',
        server_name => $server_name
   }
 
   # this command runs a program $ARG1$ with no arguments
   nagios::type::command {
     'check_nrpe_1arg':
-       command_line => "${nagios::nrpe::plugindir}/check_nrpe -H $HOSTADDRESS$ -c $ARG1$",
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$',
        server_name => $server_name
   }
   nagios::type::command {
     'check_nrpe_1arg_port':
-       command_line => "${nagios::nrpe::plugindir}/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -p $ARG2$",
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -p $ARG2$',
        server_name => $server_name
   }
 }
