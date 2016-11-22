3e68985d23df9d202dba452309836752f0c5f810
Yoann Laissus <yoann.laissus@gmail.com>
2016-11-22 22:20:36 +0100

    Delete useless package definitions.

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -2,11 +2,6 @@ class nagios::debian inherits nagios::base {
 
     Package['nagios'] { name => 'nagios3' }
 
-    package { [ 'nagios-plugins', 'nagios-snmp-plugins','nagios-nrpe-plugin' ]:
-        ensure => 'present',
-        notify => Service['nagios'],
-    }
-
     Service['nagios'] {
         name => 'nagios3',
         hasstatus => true,
