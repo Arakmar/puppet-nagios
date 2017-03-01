6a933dc0225f7ce69f62f1d5d9fe69e7085ff573
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2017-03-01 15:51:10 +0100

    Fix some remaining server_name parameters which are server_names

--- manifests/service/http.pp
+++ manifests/service/http.pp
@@ -13,7 +13,7 @@ define nagios::service::http(
     $auth_name = "",
     $auth_password = "",
     $redirect_status = 'ok',
-    $server_name = undef
+    $server_names = undef
 ){
 
         if $ssl_mode {
@@ -22,7 +22,7 @@ define nagios::service::http(
                                         host_name => $::fqdn,
                                         use => $use,
                                         service_description => "Check https of ${check_domain}${check_url} with authentification",
-                                        server_name => $server_name,
+                                        server_names => $server_names,
                                         check_command => "check_https_auth_string!${check_domain}!${check_url}!'${check_string}'!${auth_name}!${auth_password}!${redirect_status}",
                                 }
                         }
@@ -31,7 +31,7 @@ define nagios::service::http(
                                         host_name => $::fqdn,
                                         use => $use,
                                         service_description => "Check https of ${check_domain}${check_url}",
-                                        server_name => $server_name,
+                                        server_names => $server_names,
                                         check_command => "check_https_string!${check_domain}!${check_url}!'${check_string}'!${redirect_status}",
                                 }
                         }
@@ -39,7 +39,7 @@ define nagios::service::http(
                                         host_name => $::fqdn,
                                         use => $use,
                                         service_description => "Check cert of ${check_domain}${check_url}",
-                                        server_name => $server_name,
+                                        server_names => $server_names,
                                         check_command => "check_https_cert!${check_domain}!${check_url}",
                         }
         } else {
@@ -48,7 +48,7 @@ define nagios::service::http(
                                         host_name => $::fqdn,
                                         use => $use,
                                         service_description => "Check http of ${check_domain}${check_url} with authentification",
-                                        server_name => $server_name,
+                                        server_names => $server_names,
                                         check_command => "check_http_auth_string!${check_domain}!${check_url}!'${check_string}'!${auth_name}!${auth_password}!${redirect_status}",
                                 }
                         }
@@ -57,7 +57,7 @@ define nagios::service::http(
                                         host_name => $::fqdn,
                                         use => $use,
                                         service_description => "Check http of ${check_domain}${check_url}",
-                                        server_name => $server_name,
+                                        server_names => $server_names,
                                         check_command => "check_http_string!${check_domain}!${check_url}!'${check_string}'!${redirect_status}",
                                 }
                         }
