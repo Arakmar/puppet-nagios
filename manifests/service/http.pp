398414b4b96ae36d9e83f7b251c2f984c5495e7a
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2017-03-20 16:40:19 +0100

    Fix http services

--- manifests/service/http.pp
+++ manifests/service/http.pp
@@ -23,8 +23,7 @@ define nagios::service::http (
         use                 => $use,
         service_description => "Check https of ${check_domain}${check_url} with authentification",
         server_names        => $server_names,
-        check_command       => "check_https_auth_content!${check_domain}!${check_url}!'${check_string}'!${auth_name}!
-          auth_password!${redirect_status}",
+        check_command       => "check_https_auth_content!${check_domain}!${check_url}!'${check_string}'!${auth_name}!auth_password!${redirect_status}",
       }
     }
     else {
@@ -33,8 +32,7 @@ define nagios::service::http (
         use                 => $use,
         service_description => "Check https of ${check_domain}${check_url}",
         server_names        => $server_names,
-        check_command       => "check_https_url_content!${check_domain}!${check_url}!'${check_string}'!${redirect_status}
-          ",
+        check_command       => "check_https_url_content!${check_domain}!${check_url}!'${check_string}'!${redirect_status}",
       }
     }
     nagios::type::service { "https_${name}_${check_string}_cert":
@@ -51,8 +49,7 @@ define nagios::service::http (
         use                 => $use,
         service_description => "Check http of ${check_domain}${check_url} with authentification",
         server_names        => $server_names,
-        check_command       => "check_http_auth_content!${check_domain}!${check_url}!'${check_string}'!${auth_name}!
-          auth_password!${redirect_status}",
+        check_command       => "check_http_auth_content!${check_domain}!${check_url}!'${check_string}'!${auth_name}!auth_password!${redirect_status}",
       }
     }
     else {
@@ -61,8 +58,7 @@ define nagios::service::http (
         use                 => $use,
         service_description => "Check http of ${check_domain}${check_url}",
         server_names        => $server_names,
-        check_command       => "check_http_url_content!${check_domain}!${check_url}!'${check_string}'!${redirect_status}
-          ",
+        check_command       => "check_http_url_content!${check_domain}!${check_url}!'${check_string}'!${redirect_status}",
       }
     }
   }
