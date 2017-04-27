35db2867c0d29cabd3ec24c5f1bdfd45a1ebc779
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2017-04-27 09:54:32 +0200

    Add the ability to use a custom template file

--- manifests/defaults/templates.pp
+++ manifests/defaults/templates.pp
@@ -1,9 +1,18 @@
-class nagios::defaults::templates {
+class nagios::defaults::templates(
+  $source = undef,
+) {
+
+  $real_source = $source ? {
+    undef => [
+      "puppet:///modules/nagios/configs/${::operatingsystem}/nagios_templates.cfg",
+      'puppet:///modules/nagios/configs/nagios_templates.cfg',
+    ],
+    default => $source,
+  }
 
   file { 'nagios_templates':
     path   => "${nagios::params::cfg_dir}/conf.d/nagios_templates.cfg",
-    source => [ "puppet:///modules/nagios/configs/${::operatingsystem}/nagios_templates.cfg",
-      'puppet:///modules/nagios/configs/nagios_templates.cfg' ],
+    source => $real_source,
     notify => Service[$nagios::params::service],
     mode   => '0644',
     owner  => 'root',
